31f30cc37895663fab3002f2f994269290aa51fa
sarah-afiya <sarah.afiya@gmail.com>
2023-02-06 16:07:07 -0700

    adde all tests

--- src/main/java/Utilities.java
+++ src/main/java/Utilities.java
@@ -1,2 +1,25 @@
+import models.Address;
+import services.AddressService;
+import services.SalaryService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Utilities {
+
+
+    public List<Address> getAddressByCity(AddressService addressService, String city) {
+        List<Address> allAddresses = addressService.getAllAddresses();
+        List<Address> filteredAddresses = allAddresses.stream().filter(x -> x.getCity().equals(city)).collect(Collectors.toList());
+
+        return filteredAddresses;
+    }
+
+
+    public List<Integer> getSalariesAbove(SalaryService salaryService, int cutOff) {
+        List<Integer> salaries = salaryService.getSalaries();
+        List<Integer> filteredSalaries = salaries.stream().filter(x -> x > cutOff).collect(Collectors.toList());
+
+        return filteredSalaries;
+    }
 }
--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -7,6 +7,22 @@ public class Address {
     String address;
     String city;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public Address(String address, String city) {
         this.address = address;
         this.city = city;
